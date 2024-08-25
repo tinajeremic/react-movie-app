495ec0597e2ff74ee934c8105ac085718a96a629
Tina Jeremic <tinajeremic3@gmail.com>
2024-08-25 12:14:43 +0200

    API movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import {useEffect} from 'react';
+
+//b2abe3d8
+
+const API_URL = 'http://www.omdbapi.com?apikey=b2abe3d8';
 
 const App = () => {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
+
+  useEffect(() => {
+    searchMovies('Batman');
+  }, []);
+
   return (
     <h1>App</h1>
   );
